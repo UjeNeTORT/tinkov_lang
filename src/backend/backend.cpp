0fce42e22c90c1336429aa1952d6dcc5e01b2ea0
your_username <tikhonovty@gmail.com>
2023-12-21 02:08:43 +0300

    basic backend: assignment, operators, statement  sequences

---
+++ src/backend/backend.cpp
@@ -0,0 +1,308 @@
+/*************************************************************************
+ * (c) 2023 Tikhonov Yaroslav (aka UjeNeTORT)
+ *
+ * email:    tikhonovty@gmail.com
+ * telegram: https://t.me/netortofficial
+ * GitHub:   https://github.com/UjeNeTORT
+ * repo:     https://github.com/UjeNeTORT/language
+ *************************************************************************/
+
+#include "backend.h"
+
+int main (int argc, char* argv[])
+{
+    if (argc < 2)
+        RET_ERROR (1, "No ast specified");
+
+    char* ast_name = argv[1]; // todo check if .tree in the end
+
+    FILE* tree_file = fopen (ast_name, "rb");
+    Tree* ast = ReadTree (tree_file);
+    fclose (tree_file);
+    if (!ast)
+        RET_ERROR (1, "Error during reading tree");
+
+    TreeDotDump ("dump.html", ast);
+
+    AsmText* asm_text = TranslateAST (ast);
+    TreeDtor (ast);
+
+    printf ("%s\n", asm_text->text);
+
+    AsmTextDtor (asm_text);
+
+    return 0;
+}
+
+// ============================================================================================
+
+AsmText* TranslateAST (const Tree* ast)
+{
+    assert (ast);
+
+    // TODO  move this block outside the func (to main maybe)
+    AsmText* asm_text = AsmTextCtor();
+
+    TranslateASTSubtree (ast->root, asm_text, ast);
+
+    return asm_text;
+}
+
+// ============================================================================================
+
+TranslateRes TranslateASTSubtree (const TreeNode* node, AsmText* asm_text, const Tree* ast)
+{
+    ASSERT_BLOCK_FUNC_TRANSLATE;
+
+    if (!node)
+    {
+        WARN ("[%d] node (nil)", asm_text->offset);
+
+        return TRANSLATE_SUCCESS;
+    }
+
+    if (TranslateDeclarator (node, asm_text, ast) == TRANSLATE_SUCCESS)
+        return TRANSLATE_SUCCESS;
+
+    if (TranslateKeyword (node, asm_text, ast) == TRANSLATE_SUCCESS)
+        return TRANSLATE_SUCCESS;
+
+    if (TranslateSeparator (node, asm_text, ast) == TRANSLATE_SUCCESS)
+        return TRANSLATE_SUCCESS;
+
+    if (TranslateOperator (node, asm_text, ast) == TRANSLATE_SUCCESS)
+        return TRANSLATE_SUCCESS;
+
+    if (TranslateIdentifier (node, asm_text, ast) == TRANSLATE_SUCCESS)
+        return TRANSLATE_SUCCESS;
+
+    if (TranslateNumber (node, asm_text, ast) == TRANSLATE_SUCCESS)
+        return TRANSLATE_SUCCESS;
+
+    ERROR ("Couldnt translate node:\n"
+           "[type = %d]\n"
+           "[val  = %d]\n"
+           "%s", VAL (node), TYPE (node), asm_text->text);
+
+    return TRANSLATE_ERROR;
+}
+
+// ============================================================================================
+
+TranslateRes TranslateDeclarator (const TreeNode* declr_node, AsmText* asm_text, const Tree* ast)
+{
+    ASSERT_BLOCK_FUNC_TRANSLATE;
+    if (TYPE (declr_node) != DECLARATOR)
+        return TRANSLATE_TYPE_NOT_MATCH;
+
+    if (VAL (declr_node) == VAR_DECLARATOR)
+    {
+        if (!declr_node->left && !declr_node->right)
+            RET_ERROR (TRANSLATE_ERROR, "Declarator node has no children");
+
+        // AddIdToRAM (VAL (declr_node->left->left), asm_text);
+
+        return TranslateASTSubtree (declr_node->left, asm_text, ast);
+    }
+
+    return TRANSLATE_ERROR;
+}
+
+// ============================================================================================
+
+TranslateRes TranslateKeyword (const TreeNode* kw_node, AsmText* asm_text, const Tree* ast)
+{
+    ASSERT_BLOCK_FUNC_TRANSLATE;
+    if (TYPE (kw_node) != KEYWORD)
+        return TRANSLATE_TYPE_NOT_MATCH;
+
+    return TRANSLATE_ERROR;
+}
+
+// ============================================================================================
+
+TranslateRes TranslateSeparator (const TreeNode* sep_node, AsmText* asm_text, const Tree* ast)
+{
+    ASSERT_BLOCK_FUNC_TRANSLATE;
+    if (TYPE (sep_node) != SEPARATOR)
+        return TRANSLATE_TYPE_NOT_MATCH;
+
+    if (VAL (sep_node) == END_STATEMENT)
+    {
+        if (sep_node->left)
+            if (TranslateASTSubtree (sep_node->left, asm_text, ast) != TRANSLATE_SUCCESS)
+                return TRANSLATE_ERROR;
+
+        if (sep_node->right)
+            return TranslateASTSubtree (sep_node->right, asm_text, ast);
+
+        return TRANSLATE_ERROR;
+    }
+
+    else if (VAL (sep_node) == ENCLOSE_STATEMENT_BEGIN)
+    {
+        if (sep_node->left)
+            return TranslateASTSubtree (sep_node->left, asm_text, ast);
+
+        return TRANSLATE_ERROR;
+    }
+
+    return TRANSLATE_ERROR;
+}
+
+// ============================================================================================
+
+TranslateRes TranslateOperator (const TreeNode* op_node, AsmText* asm_text, const Tree* ast)
+{
+    ASSERT_BLOCK_FUNC_TRANSLATE;
+    if (TYPE (op_node) != OPERATOR)
+        return TRANSLATE_TYPE_NOT_MATCH;
+
+    if (VAL (op_node) == ASSIGN)
+    {
+        TranslateASTSubtree (op_node->right, asm_text, ast);
+
+        AddIdToRAM (VAL (op_node->left), asm_text);
+
+        int written = 0;
+        sprintf (TEXT, "pop [%d] ; assign var_%d\n%n", IN_RAM (op_node->left), VAL (op_node->left), &written);
+        asm_text->offset += written;
+    }
+
+    else if (VAL (op_node) == ADD)
+    {
+        TranslateASTSubtree (op_node->left, asm_text, ast);
+        TranslateASTSubtree (op_node->right, asm_text, ast);
+
+        int written = 0;
+        sprintf (TEXT, "add\n%n", &written);
+        asm_text->offset += written;
+    }
+
+    else if (VAL (op_node) == SUB)
+    {
+        TranslateASTSubtree (op_node->left, asm_text, ast);
+        TranslateASTSubtree (op_node->right, asm_text, ast);
+
+        int written = 0;
+        sprintf (TEXT, "sub\n%n", &written);
+        asm_text->offset += written;
+    }
+
+    else if (VAL (op_node) == MUL)
+    {
+        TranslateASTSubtree (op_node->left, asm_text, ast);
+        TranslateASTSubtree (op_node->right, asm_text, ast);
+
+        int written = 0;
+        sprintf (TEXT, "mul\n%n", &written);
+        asm_text->offset += written;
+    }
+
+    else if (VAL (op_node) == DIV)
+    {
+        TranslateASTSubtree (op_node->left, asm_text, ast);
+        TranslateASTSubtree (op_node->right, asm_text, ast);
+
+        int written = 0;
+        sprintf (TEXT, "div\n%n", &written);
+        asm_text->offset += written;
+    }
+
+    else
+    {
+        ERROR ("Operator %d not supported, be smarter please", VAL (op_node));
+
+        return TRANSLATE_ERROR;
+    }
+
+    int written = 0;
+    sprintf (TEXT, "\n%n", &written);
+    asm_text->offset += written;
+
+    return TRANSLATE_SUCCESS;
+}
+
+// ============================================================================================
+
+TranslateRes TranslateIdentifier (const TreeNode* id_node, AsmText* asm_text, const Tree* ast)
+{
+    ASSERT_BLOCK_FUNC_TRANSLATE;
+    if (TYPE (id_node) != IDENTIFIER)
+        return TRANSLATE_TYPE_NOT_MATCH;
+
+    if (IN_RAM (id_node) == -1)
+        AddIdToRAM (VAL (id_node), asm_text);
+
+    int written = 0;
+    sprintf (TEXT, "push [%d] ; get value of var_%d\n%n", IN_RAM (id_node), VAL (id_node), &written);
+    asm_text->offset += written;
+
+    return TRANSLATE_SUCCESS;
+}
+
+// ============================================================================================
+
+TranslateRes TranslateNumber (const TreeNode* num_node, AsmText* asm_text, const Tree* ast)
+{
+    ASSERT_BLOCK_FUNC_TRANSLATE;
+    if (TYPE (num_node) != INT_LITERAL)
+        return TRANSLATE_TYPE_NOT_MATCH;
+
+    int written = 0;
+    sprintf (TEXT, "push %d\n%n", VAL (num_node), &written);
+
+    asm_text->offset += written;
+
+    return TRANSLATE_SUCCESS;
+}
+
+// ============================================================================================
+
+int AddIdToRAM (int identifier_index, AsmText* asm_text)
+{
+    /**
+     * | id_1 | id_2 | id_3 |
+     * ----------------------
+     * | rm_2 | rm_1 |  -1  |
+    */
+
+    assert (-1 < identifier_index);
+    assert (asm_text);
+
+    if (asm_text->ram_table.index_in_ram[identifier_index] == -1)
+        asm_text->ram_table.index_in_ram[identifier_index] = asm_text->ram_table.free_ram_index;
+
+    return asm_text->ram_table.free_ram_index++;
+}
+
+// ============================================================================================
+
+AsmText* AsmTextCtor ()
+{
+    AsmText* asm_text = (AsmText*) calloc (1, sizeof (AsmText));
+
+    asm_text->text = (char*) calloc (MAX_ASM_PROGRAM_SIZE, sizeof (char));
+    asm_text->offset = 0;
+
+    for (int i = 0; i < NAMETABLE_CAPACITY; i++)
+        asm_text->ram_table.index_in_ram[i] = -1;
+
+    asm_text->ram_table.free_ram_index = 0;
+
+    return asm_text;
+}
+
+// ============================================================================================
+
+int AsmTextDtor (AsmText* asm_text)
+{
+    assert (asm_text);
+
+    asm_text->offset = -1;
+
+    free (asm_text->text);
+    free (asm_text);
+
+    return 0;
+}
---
+++ src/backend/backend.h
@@ -0,0 +1,79 @@
+/*************************************************************************
+ * (c) 2023 Tikhonov Yaroslav (aka UjeNeTORT)
+ *
+ * email:    tikhonovty@gmail.com
+ * telegram: https://t.me/netortofficial
+ * GitHub:   https://github.com/UjeNeTORT
+ * repo:     https://github.com/UjeNeTORT/language
+ *************************************************************************/
+
+#ifndef TINKOV_BACKEND_H
+#define TINKOV_BACKEND_H
+
+#include <assert.h>
+#include <stdio.h>
+
+#include "../common/common.h"
+#include "../tree/tree.h"
+#include "../tree/tree_dump/tree_dump.h"
+#include "../processor/assembler/asm.h"
+#include "../processor/processor/spu.h"
+
+// =========================== DSL ===========================
+
+#define ASSERT_BLOCK_FUNC_TRANSLATE \
+    assert (asm_text);              \
+    assert (asm_text->text);        \
+    assert (ast);                   \
+    assert (ast->root);             \
+    assert (ast->nametable);
+
+#define IN_RAM(node) asm_text->ram_table.index_in_ram[VAL (node)]
+
+#define TEXT (asm_text->text + asm_text->offset)
+
+#define NODE_IS(type, val) \
+    (TYPE (node) == (type) && VAL (node) == (val))
+
+#define NODE_IS_NOT(type, val) \
+    (TYPE (node) != (type) || VAL (node) != (val))
+// ===========================================================
+
+const size_t MAX_ASM_PROGRAM_SIZE = 50000;
+
+struct RamTable
+{
+    int index_in_ram[NAMETABLE_CAPACITY];
+    int free_ram_index;
+};
+
+struct AsmText
+{
+    char* text;
+    int   offset;
+    RamTable ram_table; // table to specify location in ram where identifier values are stored
+};
+
+typedef enum
+{
+    TRANSLATE_SUCCESS        = 0,
+    TRANSLATE_TYPE_NOT_MATCH = 1,
+    TRANSLATE_ERROR          = 2,
+} TranslateRes;
+
+AsmText*     TranslateAST (const Tree* ast);
+TranslateRes TranslateASTSubtree (const TreeNode* node, AsmText* asm_text, const Tree* ast);
+
+TranslateRes TranslateDeclarator (const TreeNode* declr_node, AsmText* asm_text, const Tree* ast);
+TranslateRes TranslateKeyword    (const TreeNode* kw_node,    AsmText* asm_text, const Tree* ast);
+TranslateRes TranslateSeparator  (const TreeNode* sep_node,   AsmText* asm_text, const Tree* ast);
+TranslateRes TranslateOperator   (const TreeNode* op_node,    AsmText* asm_text, const Tree* ast);
+TranslateRes TranslateIdentifier (const TreeNode* id_node,    AsmText* asm_text, const Tree* ast);
+TranslateRes TranslateNumber     (const TreeNode* num_node,   AsmText* asm_text, const Tree* ast);
+
+int AddIdToRAM (int identifier_index, AsmText* ast);
+
+AsmText* AsmTextCtor ();
+int      AsmTextDtor (AsmText* asm_text);
+
+#endif // TINKOV_BACKEND_H
