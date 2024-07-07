b5f7233043801d31cc9035a678dfa45856d079fa
ujenetort <tikhonovty@gmail.com>
2024-07-07 23:25:28 +0300

    fix: builder now can create nessesary dirs

--- build.sh
+++ build.sh
@@ -1,1 +1,12 @@
+# create dir for obj files 
 mkdir -p obj/
+
+# create dir for dot files 
+mkdir -p src/tree/tree_dump/
+mkdir -p src/tree/tree_dump/dumps
+mkdir -p src/tree/tree_dump/dumps/png
+mkdir -p src/tree/tree_dump/dumps/dot
+mkdir -p src/tree/tree_dump/dumps/dumps
+mkdir -p src/tree/tree_dump/dumps/tex
+mkdir -p src/tree/tree_dump/dumps/pdf
+
